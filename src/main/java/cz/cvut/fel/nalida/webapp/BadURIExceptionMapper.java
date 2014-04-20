37dbd42cc1c41321ed41b0372b0610b05be2fa27
Jakub Stejskal <jakubstejskal22@gmail.com>
2014-04-20 17:46:28 +0200

    Print error message when invalid path accessed.

---
+++ src/main/java/cz/cvut/fel/nalida/webapp/BadURIExceptionMapper.java
@@ -0,0 +1,16 @@
+package cz.cvut.fel.nalida.webapp;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.sun.jersey.api.NotFoundException;
+
+@Provider
+public class BadURIExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+	@Override
+	public Response toResponse(NotFoundException exception) {
+		return Response.status(Response.Status.NOT_FOUND).entity("URI " + exception.getNotFoundUri() + " not found.	").build();
+	}
+}
