bb0b84b80c188b88db7efefce8187ecfaf4caa34
Jakub Stejskal <jakubstejskal22@gmail.com>
2014-05-04 18:08:29 +0200

    URI not found error message in HTML format has link to homepage.

--- src/main/java/cz/cvut/fel/nalida/webapp/BadURIExceptionMapper.java
+++ src/main/java/cz/cvut/fel/nalida/webapp/BadURIExceptionMapper.java
@@ -1,19 +1,37 @@
 package cz.cvut.fel.nalida.webapp;
 
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 import com.sun.jersey.api.NotFoundException;
+import com.sun.jersey.api.core.ExtendedUriInfo;
 
 @Provider
 public class BadURIExceptionMapper implements ExceptionMapper<NotFoundException> {
 
+	@Context
+	private HttpHeaders headers;
+
+	@Context
+	private ExtendedUriInfo uriInfo;
+
 	@Override
 	public Response toResponse(NotFoundException exception) {
-		return Response.status(Response.Status.NOT_FOUND)
-				.entity(new ErrorBean("URI " + exception.getNotFoundUri() + " not found.", Response.Status.NOT_FOUND.getStatusCode()))
-				.build();
+		if (this.headers.getMediaType() == null || this.headers.getMediaType().equals(MediaType.TEXT_HTML)) {
+			return Response
+					.status(Response.Status.NOT_FOUND)
+					.type(MediaType.TEXT_HTML)
+					.entity("<h1>Page not found</h1><p>URI " + exception.getNotFoundUri()
+							+ " not found.</p><p>Continue to the <a href=\"/nalida-web\">home page</a>.</p>").build();
+		} else {
+			return Response.status(Response.Status.NOT_FOUND)
+					.entity(new ErrorBean("URI " + exception.getNotFoundUri() + " not found.", Response.Status.NOT_FOUND.getStatusCode()))
+					.build();
+		}
 
 	}
 }
